8970517546a5a901b4441f9d07718d91b0b00b60
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-06-18 05:11:28 +0200

    Reformat

--- customer-api-provider/src/test/java/de/schulung/sample/quarkus/CustomerApiTests.java
+++ customer-api-provider/src/test/java/de/schulung/sample/quarkus/CustomerApiTests.java
@@ -16,81 +16,80 @@ import static org.hamcrest.Matchers.*;
 @QuarkusTest
 class CustomerApiTests {
 
-    @Nested
-    @DisplayName("GET /customers")
-    class GetCustomersTests {
-        /*
-         * GET /customers, Accept: application/json
-         * -> 200, Content-Type: application/json
-         */
-        @Test
-        void shouldReturn200WhenGetCustomers() {
-            given()
-                    .when()
-                    .accept(ContentType.JSON)
-                    .get("/customers")
-                    .then()
-                    .statusCode(200)
-                    .contentType(ContentType.JSON)
-                    .body(startsWith("["))
-                    .body(endsWith("]"));
-            // we could also test for JSON schema: https://medium.com/@dhadiprasetyo/asserting-json-schema-for-api-testing-with-java-and-restassured-79b4a851f282
-        }
-
-        /*
-         * GET /customers, Accept: application/xml
-         * -> 406
-         */
-        @Test
-        void shouldReturn406WhenGetCustomersWithInvalidContentType() {
-            given()
-                    .when()
-                    .accept(ContentType.XML)
-                    .get("/customers")
-                    .then()
-                    .statusCode(406);
-        }
+  @Nested
+  @DisplayName("GET /customers")
+  class GetCustomersTests {
+    /*
+     * GET /customers, Accept: application/json
+     * -> 200, Content-Type: application/json
+     */
+    @Test
+    void shouldReturn200WhenGetCustomers() {
+      given()
+        .when()
+        .accept(ContentType.JSON)
+        .get("/customers")
+        .then()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        .body(startsWith("["))
+        .body(endsWith("]"));
+      // we could also test for JSON schema: https://medium.com/@dhadiprasetyo/asserting-json-schema-for-api-testing-with-java-and-restassured-79b4a851f282
     }
 
-
-    @Nested
-    @DisplayName("POST /customers")
-    class CreateCustomerTests {
-
-        @Test
-        void shouldCreateCustomer() {
-            var location = given()
-                    .when()
-                    .contentType(ContentType.JSON)
-                    .body("""
-                            {
-                                "name": "Tom",
-                                "birthdate": "2000-10-04",
-                                "state": "active"
-                            }
-                            """)
-                    .accept(ContentType.JSON)
-                    .post("/customers")
-                    .then()
-                    .statusCode(201)
-                    .header("Location", is(notNullValue()))
-                    .header("Location", startsWith("http"))
-                    .contentType(ContentType.JSON)
-                    .body("name", is(equalTo("Tom")))
-                    .body("birthdate", is(equalTo("2000-10-04")))
-                    .body("uuid", is(notNullValue()))
-                    .extract()
-                    .header("Location");
-
-            given()
-                    .when()
-                    .accept(ContentType.JSON)
-                    .get(location)
-                    .then()
-                    .statusCode(200)
-                    .body("name", is(equalTo("Tom")))
-                    .body("birthdate", is(equalTo("2000-10-04")));
-        }
-
+    /*
+     * GET /customers, Accept: application/xml
+     * -> 406
+     */
+    @Test
+    void shouldReturn406WhenGetCustomersWithInvalidContentType() {
+      given()
+        .when()
+        .accept(ContentType.XML)
+        .get("/customers")
+        .then()
+        .statusCode(406);
     }
+  }
+
+  @Nested
+  @DisplayName("POST /customers")
+  class CreateCustomerTests {
+
+    @Test
+    void shouldCreateCustomer() {
+      var location = given()
+        .when()
+        .contentType(ContentType.JSON)
+        .body("""
+          {
+              "name": "Tom",
+              "birthdate": "2000-10-04",
+              "state": "active"
+          }
+          """)
+        .accept(ContentType.JSON)
+        .post("/customers")
+        .then()
+        .statusCode(201)
+        .header("Location", is(notNullValue()))
+        .header("Location", startsWith("http"))
+        .contentType(ContentType.JSON)
+        .body("name", is(equalTo("Tom")))
+        .body("birthdate", is(equalTo("2000-10-04")))
+        .body("uuid", is(notNullValue()))
+        .extract()
+        .header("Location");
+
+      given()
+        .when()
+        .accept(ContentType.JSON)
+        .get(location)
+        .then()
+        .statusCode(200)
+        .body("name", is(equalTo("Tom")))
+        .body("birthdate", is(equalTo("2000-10-04")));
+    }
+
+  }
 }
