395ce626f7b0071f4550c7534c0d3b8adca51910
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-06-18 05:08:04 +0200

    Reformat, add .dockerignore

--- customer-api-provider/src/main/java/de/schulung/sample/quarkus/CustomerApplication.java
+++ customer-api-provider/src/main/java/de/schulung/sample/quarkus/CustomerApplication.java
@@ -6,8 +6,8 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class CustomerApplication {
 
-    public static void main(String[] args) {
-        Quarkus.run(args);
-    }
+  public static void main(String[] args) {
+    Quarkus.run(args);
+  }
 
 }
